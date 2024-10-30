0edf4282ea53edf9924d0d0e87d6737f974b33e4
dalcheonroadhead <wjsaos2081@gmail.com>
2024-10-31 01:01:36 +0900

    first commit

---
+++ jejutanslation/src/main/java/org/groom/moongte/jejutanslation/JejutanslationApplication.java
@@ -0,0 +1,13 @@
+package org.groom.moongte.jejutanslation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JejutanslationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JejutanslationApplication.class, args);
+    }
+
+}
