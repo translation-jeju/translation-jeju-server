95df31b98ed7023747ada76b016a4593e8a1901d
dalcheonroadhead <wjsaos2081@gmail.com>
2024-10-31 13:35:08 +0900

    chore: backend Setting

--- jejutanslation/src/main/java/org/groom/moongte/jejutanslation/JejutanslationApplication.java
+++ jejutanslation/src/main/java/org/groom/moongte/jejutanslation/JejutanslationApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JejutanslationApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(JejutanslationApplication.class, args);
     }
-
 }
