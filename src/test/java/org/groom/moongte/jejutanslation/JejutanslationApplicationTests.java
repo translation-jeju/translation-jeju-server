3266c2e7f11b6f6d770a0e64033050011e7bdf99
dalcheonroadhead <wjsaos2081@gmail.com>
2024-10-31 14:02:24 +0900

    chore: backend Setting

---
+++ src/test/java/org/groom/moongte/jejutanslation/JejutanslationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.groom.moongte.jejutanslation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JejutanslationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
